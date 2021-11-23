f3fd5bd7be19fa7e84a79b3a450432ae5198ec0c
rups20097 <rups20097@gmail.com>
2021-11-23 23:53:48 +0530

    bug fix and new css changes

--- my_page_mobile.js
+++ my_page_mobile.js
@@ -8,16 +8,22 @@
 //         $('#change_view_for_mobile').addClass('col-6')
 //     }
 // });
-$( document ).ready(() => {
-    if ($(window).width() < 768) {
-        console.log('THIS')
-        let x = 
-            `<div class="" class="col-xs-12 col-md-6" style="margin-top: 200px;">
+$(document).ready(() => {
+  if ($(window).width() < 768) {
+    console.log("THIS");
+    let x = `<div class="" class="col-xs-12 col-md-6" style="margin-top: 200px;">
                 <img style="width: 100%; border-radius: 20px;" src="./images/me_laptop.JPEG">
             </div>
             <div class="img_margin" class="col-xs-12 col-md-6" style="margin-top: 200px;">
             <div style="font-size: 30px;" class="m_b_20">Hello folks,</div>
-            <p class="m_b_20 font_w_light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p class="m_b_20">
+                I am a passionate worker. Whether its an art or my hobbies or maybe its a development, I give my best.
+                I believe, <br/>
+                <p class="quoted">an Artist always try to make things perfect.</p><br/> Its their virtue. Thus according to them mistakes should be tending to zero for any particular work.
+                Thats the golden rule I follow in every work.
+                I don't develop because its my job or I have to, but I Love to.
+                I have my dreams my passions my goals with developments and I am always excited about it. Thus I never feel saturated in working.
+            </p>
             <a target="_blank" href="./files/CV_2020.pdf" download>    
                 <button id="download_cv" class="cv_btn">Download CV
                     <svg xmlns="http://www.w3.org/2000/svg" width="15.674" height="16.762" viewBox="0 0 15.674 16.762">
@@ -26,6 +32,6 @@ $( document ).ready(() => {
                 </button>
             </a>
         </div>`;
-        $('#me_laptop_img_box').html(x)
-    }
-})
+    $("#me_laptop_img_box").html(x);
+  }
+});
